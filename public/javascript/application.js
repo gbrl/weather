4dc4f32be675c022a12435b1e2ced2de69cda3f1
Gabriel Rambert <gabriel.rambert@gmail.com>
2016-06-01 18:45:48 -0400

    Got list of cities with API call for each returning json

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -6,9 +6,14 @@ $(document).ready(function() {
     var query = $(this).children("#name").val();
     var query_url = city_search_url + query;
     console.log(query_url);
-    $.get(query_url, function( data ) {
-        console.log(data);
+    $.get(query_url,function( data ) {
+      var results = jQuery.parseJSON( data );
+      var cities = results["RESULTS"];
+      console.log(cities);
+      $.each(cities, function( index, value ) {
+        $("ol").append("<li><a href='http://api.wunderground.com/api/1e0a68802ba3c6ed/conditions/q/"+ value["lat"] + "," + value["lon"] + ".json'>" + value["name"] + "</a></li>");
       });
+    });
   });
 
 });
